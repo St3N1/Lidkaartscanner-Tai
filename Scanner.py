f2991cf2af064e629538802da6b76db284be2916
Sten H <94611203+Sten-AP@users.noreply.github.com>
2023-05-26 11:11:24 +0200

    echt finaal

--- Scanner.py
+++ Scanner.py
@@ -4,6 +4,7 @@ import cv2
 from zxingcpp import read_barcodes
 import numpy as np
 from pyzbar.pyzbar import decode
+import time
 
 def aanwezigheid_toevoegen(achternaam, voornaam, datum):
     try:
@@ -31,46 +32,49 @@ def open_database():
                     Datum DATE NOT NULL, UNIQUE(Voornaam, Achternaam, Datum));''')
 
 
+def decoder(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    barcodes = read_barcodes(gray)
+    barcode = decode(gray)
+
+    gegevens = ""
+    for code in barcodes:
+        gegevens = code.text
+
+    for obj in barcode:
+        points = obj.polygon
+        (x, y, w, h) = obj.rect
+        pts = np.array(points, np.int32)
+        pts = pts.reshape((-1, 1, 2))
+        cv2.polylines(frame, [pts], True, (0, 255, 0), 3)
+
+        if gegevens != "":
+            voornaam = ""
+            achternaam = ""
+            switch = False
+            for i, letter in enumerate(gegevens):
+                if i > 3:
+                    if letter == "_":
+                        switch = True
+                    elif switch:
+                        voornaam += letter
+                    else:
+                        achternaam += letter
+
+            if achternaam[0] != "A":
+                open_database()
+                aanwezigheid_toevoegen(
+                    voornaam, achternaam, date.strftime("%d/%m/%Y"))
+                cv2.putText(frame, f"{voornaam} {achternaam}", (x, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2)
+
 def scanner():
     cap = cv2.VideoCapture("http://192.168.1.178:4000/video")
     while True:
         ret, frame = cap.read()
+        decoder(frame)
         
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        barcodes = read_barcodes(gray)
-        barcode = decode(gray)
-        
-        gegevens = ""
-        for code in barcodes:
-            gegevens = code.text
-
-        for obj in barcode:
-            points = obj.polygon
-            (x, y, w, h) = obj.rect
-            pts = np.array(points, np.int32)
-            pts = pts.reshape((-1, 1, 2))
-            cv2.polylines(frame, [pts], True, (0, 255, 0), 6)
-
-            if gegevens != "":
-                voornaam = ""
-                achternaam = ""
-                switch = False
-                for i, letter in enumerate(gegevens):
-                    if i > 3:
-                        if letter == "_":
-                            switch = True
-                        elif switch:
-                            voornaam += letter
-                        else:
-                            achternaam += letter
-
-                if achternaam[0] != "A":
-                    open_database()
-                    aanwezigheid_toevoegen(
-                        voornaam, achternaam, date.strftime("%d/%m/%Y"))
-
         cv2.imshow("Barcode Scanner", frame)
-
+        
         code = cv2.waitKey(10)
         if code == ord('q'):
             break
